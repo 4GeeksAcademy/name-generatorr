6912dec5564065765465b60ba9f627a7f29a8d94
Artyom462 <131834496+Artyom462@users.noreply.github.com>
2023-06-14 15:25:45 +0000

    fix

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = ["awesome", "cool", "intriguing", "thrilling", "funny"];
+  let nouns = ["website", "blog", "webapp", "spot", "hub"];
+  let tlds = [
+    ".com",
+    ".de",
+    ".space",
+    ".rocks",
+    ".barcelona",
+    ".org",
+    ".cat",
+    ".dog"
+  ];
+
+  let domainNames = [];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+
+  try {
+    throw new Error("oh no, something broke!");
+  } catch (error) {
+    console.log(error);
+  }
+
+  document.querySelector("#generate").onclick = () => {
+    document.querySelector("#domain").innerHTML = `<h1>${
+      domainNames[Math.floor(Math.random() * domainNames.length)]
+    }</h1>`;
+  };
 };
